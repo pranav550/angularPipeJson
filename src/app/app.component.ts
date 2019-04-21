507b297933d25f012a4e03c4c75bf928aec013d1
pranav <pranavverma550@gmail.com>
2019-04-21 08:22:30 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularJsonPipe';
+  employees=[
+    {"id":1, "name":"Rahul"},
+    {"id":2, "name":"Ravi"},
+    {"id":3, "name":"Rishi"},
+    {"id":4, "name":"Rohan"}
+  ]
 }
